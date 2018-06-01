0d60f6cb3c3ca70fc30f69a9d4ed926d608763e6
levogiakhang <levogiakhang@gmail.com>
2018-06-01 11:53:34 +0700

    create PlaceDetails module

---
+++ src/main/java/com/phuongkhanh/youmetrips/presentation/components/home/place_details/PlaceDetailsScreenImpl.java
@@ -0,0 +1,57 @@
+package com.phuongkhanh.youmetrips.presentation.components.home.place_details;
+
+import com.phuongkhanh.youmetrips.presentation.framework.FXMLScreen;
+import com.phuongkhanh.youmetrips.services.api.models.PlaceDetails;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PlaceDetailsScreenImpl extends FXMLScreen
+implements PlaceDetailsScreen, Initializable
+{
+    @Override
+    public void updatePlaceInfo(PlaceDetails placeDetails) {
+
+    }
+
+    @Override
+    public void navigateToCreateTrekkingPlan() {
+
+    }
+
+    @Override
+    public void showError(String title, String message) {
+
+    }
+
+    @Override
+    public void updateUserRating(int rate) {
+
+    }
+
+    @Override
+    public void onLiked() {
+
+    }
+
+    @Override
+    public void updateMapUrl(String mapUrl) {
+
+    }
+
+    @Override
+    public void updateUserAvatar(String avatar) {
+
+    }
+
+    @Override
+    protected String fxmlPath() {
+        return null;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
