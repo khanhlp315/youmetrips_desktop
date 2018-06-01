1fe6438a0f1343176994a4b4b463d6a91f8a5a3a
levogiakhang <levogiakhang@gmail.com>
2018-06-01 09:55:16 +0700

    update profile view

--- src/main/java/com/phuongkhanh/youmetrips/presentation/components/home/profile/ProfileScreenImpl.java
+++ src/main/java/com/phuongkhanh/youmetrips/presentation/components/home/profile/ProfileScreenImpl.java
@@ -1,4 +1,56 @@
 package com.phuongkhanh.youmetrips.presentation.components.home.profile;
 
-public class ProfileScreenImpl {
+import com.phuongkhanh.youmetrips.presentation.framework.FXMLScreen;
+import com.phuongkhanh.youmetrips.services.api.models.Friend;
+import com.phuongkhanh.youmetrips.services.api.models.Profile;
+
+import java.util.List;
+
+public class ProfileScreenImpl extends FXMLScreen
+        implements ProfileScreen
+{
+    @Override
+    public void updateProfile(Profile profile) {
+
+    }
+
+    @Override
+    public void navigateToStart() {
+
+    }
+
+    @Override
+    public void navigateToCreateTrekkingPlan() {
+
+    }
+
+    @Override
+    public void navigateToEditProfile() {
+
+    }
+
+    @Override
+    public void navigateToPlanList() {
+
+    }
+
+    @Override
+    public void navigateToFriendList() {
+
+    }
+
+    @Override
+    public void navigateToPlanDetails(int planId) {
+
+    }
+
+    @Override
+    public void updateFriends(List<Friend> friends) {
+
+    }
+
+    @Override
+    protected String fxmlPath() {
+        return null;
+    }
 }
