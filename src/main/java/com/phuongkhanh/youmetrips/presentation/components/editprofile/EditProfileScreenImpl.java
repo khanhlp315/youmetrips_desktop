e9c2c197f035c053ac8ba7561a6848265eb1c4d7
levogiakhang <levogiakhang@gmail.com>
2018-06-01 17:32:40 +0700

    fix Khanh's stupid error

---
+++ src/main/java/com/phuongkhanh/youmetrips/presentation/components/editprofile/EditProfileScreenImpl.java
@@ -0,0 +1,53 @@
+package com.phuongkhanh.youmetrips.presentation.components.editprofile;
+
+import com.phuongkhanh.youmetrips.presentation.framework.FXMLScreen;
+import com.phuongkhanh.youmetrips.services.api.models.Country;
+import com.phuongkhanh.youmetrips.services.api.models.Profile;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class EditProfileScreenImpl extends FXMLScreen
+implements EditProfileScreen, Initializable {
+    @Override
+    public void updateProfile(Profile profile) {
+        
+    }
+
+    @Override
+    public void updateAvatarUrl(String url) {
+
+    }
+
+    @Override
+    public void showNext() {
+
+    }
+
+    @Override
+    public void hideNext() {
+
+    }
+
+    @Override
+    public void setLoading(boolean value) {
+
+    }
+
+    @Override
+    public void updateCountries(List<Country> countries) {
+
+    }
+
+    @Override
+    protected String fxmlPath() {
+        return null;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
