d1e47614301017d3396dd1441cb0e282e0fdac0f
levogiakhang <levogiakhang@gmail.com>
2018-06-01 09:42:17 +0700

    update Profile class

--- src/main/java/com/phuongkhanh/youmetrips/services/api/models/UserTrekkingPlanPlace.java
+++ src/main/java/com/phuongkhanh/youmetrips/services/api/models/UserTrekkingPlanPlace.java
@@ -1,4 +1,43 @@
 package com.phuongkhanh.youmetrips.services.api.models;
 
+import java.util.List;
+
 public class UserTrekkingPlanPlace {
+    private int id;
+    private String name;
+    private String coverImageUrl;
+    private List<String> tags;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCoverImageUrl() {
+        return coverImageUrl;
+    }
+
+    public void setCoverImageUrl(String coverImageUrl) {
+        this.coverImageUrl = coverImageUrl;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
 }
