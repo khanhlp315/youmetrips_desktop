d054744e6308c52858e6fd3744c3b43bdf75f625
Lê Phương Khanh <khanhlp315@gmail.com>
2018-06-01 13:33:09 +0700

    done edit profile

---
+++ src/main/java/com/phuongkhanh/youmetrips/presentation/components/editprofile/EditProfileService.java
@@ -0,0 +1,22 @@
+package com.phuongkhanh.youmetrips.presentation.components.editprofile;
+
+import com.phuongkhanh.youmetrips.services.api.models.Country;
+import com.phuongkhanh.youmetrips.services.api.models.EditedUserProfile;
+import com.phuongkhanh.youmetrips.services.api.models.Profile;
+import com.phuongkhanh.youmetrips.services.stores.AuthenticationStore;
+import com.phuongkhanh.youmetrips.services.stores.HomeStore;
+
+import java.io.File;
+import java.util.List;
+
+public interface EditProfileService {
+    void updateUserProfile(EditedUserProfile profile, int userId, String jwt);
+    String uploadFile(int userId, String jwt, File file);
+    List<Country> getAllCountries();
+
+    AuthenticationStore getAuthenticationStore();
+
+    Profile getUserProfile(int userId, String jwt);
+
+    HomeStore getHomeStore();
+}
