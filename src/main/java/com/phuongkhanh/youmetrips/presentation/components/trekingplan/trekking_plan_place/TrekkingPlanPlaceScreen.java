a4b267ab8269c8cf22076ef9f22e58517326d8f7
levogiakhang <levogiakhang@gmail.com>
2018-06-01 14:57:40 +0700

    create new module

---
+++ src/main/java/com/phuongkhanh/youmetrips/presentation/components/trekingplan/trekking_plan_place/TrekkingPlanPlaceScreen.java
@@ -0,0 +1,17 @@
+package com.phuongkhanh.youmetrips.presentation.components.trekingplan.trekking_plan_place;
+
+import com.phuongkhanh.youmetrips.presentation.framework.JFXScreen;
+import com.phuongkhanh.youmetrips.services.api.models.Place;
+
+import java.util.List;
+
+public interface TrekkingPlanPlaceScreen extends JFXScreen {
+    void showContinue();
+    void hideContinue();
+
+    void updatePlaces(List<Place> places);
+
+    void navigateToCreateTrekkingPlace();
+
+    void updateMapUrl(String mapUrl);
+}
