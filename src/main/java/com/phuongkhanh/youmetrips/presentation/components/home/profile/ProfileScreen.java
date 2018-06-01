4a92d62d52a8ce0d9ea4531384d331dce724ac79
levogiakhang <levogiakhang@gmail.com>
2018-06-01 09:55:01 +0700

    update ProfileScreen

--- src/main/java/com/phuongkhanh/youmetrips/presentation/components/home/profile/ProfileScreen.java
+++ src/main/java/com/phuongkhanh/youmetrips/presentation/components/home/profile/ProfileScreen.java
@@ -1,7 +1,19 @@
 package com.phuongkhanh.youmetrips.presentation.components.home.profile;
 
+import com.phuongkhanh.youmetrips.services.api.models.Friend;
 import com.phuongkhanh.youmetrips.services.api.models.Profile;
+
+import java.util.List;
 
 public interface ProfileScreen {
      void updateProfile(Profile profile);
+     void navigateToStart();
+     void navigateToCreateTrekkingPlan();
+     void navigateToEditProfile();
+     void navigateToPlanList();
+     void navigateToFriendList();
+     void navigateToPlanDetails(int planId);
+
+     void updateFriends(List<Friend> friends);
+
 }
