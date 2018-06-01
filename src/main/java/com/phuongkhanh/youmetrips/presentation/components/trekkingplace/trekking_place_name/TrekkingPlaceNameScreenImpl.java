b7aee31d59fe00ac455bdde27ba695abe65b3868
levogiakhang <levogiakhang@gmail.com>
2018-06-01 13:43:42 +0700

    create TrekkingPlaceName

---
+++ src/main/java/com/phuongkhanh/youmetrips/presentation/components/trekkingplace/trekking_place_name/TrekkingPlaceNameScreenImpl.java
@@ -0,0 +1,35 @@
+package com.phuongkhanh.youmetrips.presentation.components.trekkingplace.trekking_place_name;
+
+import com.phuongkhanh.youmetrips.presentation.framework.FXMLScreen;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TrekkingPlaceNameScreenImpl extends FXMLScreen
+implements TrekkingPlaceNameScreen, Initializable {
+    @Override
+    public void showContinue() {
+
+    }
+
+    @Override
+    public void hideContinue() {
+
+    }
+
+    @Override
+    public void showError(String title, String message) {
+
+    }
+
+    @Override
+    protected String fxmlPath() {
+        return null;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
