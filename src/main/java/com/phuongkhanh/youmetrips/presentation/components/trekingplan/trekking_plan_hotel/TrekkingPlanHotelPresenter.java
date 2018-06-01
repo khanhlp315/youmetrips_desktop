8abd753a8498f75fcfd12fca670f95e40935f88d
levogiakhang <levogiakhang@gmail.com>
2018-06-01 15:00:30 +0700

    create new module

---
+++ src/main/java/com/phuongkhanh/youmetrips/presentation/components/trekingplan/trekking_plan_hotel/TrekkingPlanHotelPresenter.java
@@ -0,0 +1,20 @@
+package com.phuongkhanh.youmetrips.presentation.components.trekingplan.trekking_plan_hotel;
+
+import com.phuongkhanh.youmetrips.presentation.framework.PresenterBase;
+
+public class TrekkingPlanHotelPresenter extends PresenterBase<TrekkingPlanHotelScreen> {
+    void onStarsUpdated(int stars){
+        assert(getView() != null);
+        if (_isValidInput(stars)){
+            getView().setCanNext(true);
+        }
+        else {
+            getView().setCanNext(false);
+        }
+    }
+
+    private boolean _isValidInput(int stars) {
+        return stars != 0;
+    }
+
+}
---
+++ src/main/java/com/phuongkhanh/youmetrips/presentation/components/trekingplan/trekking_plan_hotel/TrekkingPlanHotelScreen.java
@@ -0,0 +1,8 @@
+package com.phuongkhanh.youmetrips.presentation.components.trekingplan.trekking_plan_hotel;
+
+import com.phuongkhanh.youmetrips.presentation.framework.JFXScreen;
+
+public interface TrekkingPlanHotelScreen extends JFXScreen {
+    void setCanNext(boolean value);
+    void setLoading(boolean value);
+}
