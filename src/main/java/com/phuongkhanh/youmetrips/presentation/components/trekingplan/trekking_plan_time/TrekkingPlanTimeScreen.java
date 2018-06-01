0bca990ab51958226c453b5774b4ad0d35c5f556
levogiakhang <levogiakhang@gmail.com>
2018-06-01 14:28:43 +0700

    create trekkingplanetime module and add dependency

---
+++ src/main/java/com/phuongkhanh/youmetrips/presentation/components/trekingplan/trekking_plan_time/TrekkingPlanTimeScreen.java
@@ -0,0 +1,7 @@
+package com.phuongkhanh.youmetrips.presentation.components.trekingplan.trekking_plan_time;
+
+import com.phuongkhanh.youmetrips.presentation.framework.JFXScreen;
+
+public interface TrekkingPlanTimeScreen extends JFXScreen {
+    void setCanNext(boolean value);
+}
---
+++ src/main/java/com/phuongkhanh/youmetrips/presentation/components/trekingplan/trekking_plan_time/TrekkingPlanTimeScreenImpl.java
@@ -0,0 +1,25 @@
+package com.phuongkhanh.youmetrips.presentation.components.trekingplan.trekking_plan_time;
+
+import com.phuongkhanh.youmetrips.presentation.framework.FXMLScreen;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TrekkingPlanTimeScreenImpl extends FXMLScreen
+implements TrekkingPlanTimeScreen, Initializable {
+    @Override
+    public void setCanNext(boolean value) {
+
+    }
+
+    @Override
+    protected String fxmlPath() {
+        return null;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
