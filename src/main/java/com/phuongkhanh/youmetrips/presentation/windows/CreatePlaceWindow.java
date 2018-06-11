89ce4fc0d2087bb4ecee0977c54412bc41aba497
levogiakhang <levogiakhang@gmail.com>
2018-06-11 14:42:54 +0700

    create place window

--- src/main/java/com/phuongkhanh/youmetrips/presentation/windows/CreatePlaceWindow.java
+++ src/main/java/com/phuongkhanh/youmetrips/presentation/windows/CreatePlaceWindow.java
@@ -1,4 +1,23 @@
 package com.phuongkhanh.youmetrips.presentation.windows;
 
-public class CreatePlaceWindow {
+import com.phuongkhanh.youmetrips.presentation.components.trekkingplace.trekking_place_hashtags.TrekkingPlaceHashtagsScreen;
+import com.phuongkhanh.youmetrips.presentation.components.trekkingplace.trekking_place_location.TrekkingPlaceLocationScreen;
+import com.phuongkhanh.youmetrips.presentation.components.trekkingplace.trekking_place_name.TrekkingPlaceNameScreen;
+import com.phuongkhanh.youmetrips.presentation.components.trekkingplace.trekking_place_photos.TrekkingPlacePhotosScreen;
+import com.phuongkhanh.youmetrips.presentation.framework.JFXWindowBase;
+
+import javax.inject.Inject;
+
+public class CreatePlaceWindow extends JFXWindowBase {
+
+    @Inject
+    public CreatePlaceWindow(
+            final TrekkingPlaceNameScreen nameScreen,
+            final TrekkingPlaceLocationScreen locationScreen,
+            final TrekkingPlacePhotosScreen photosScreen,
+            final TrekkingPlaceHashtagsScreen hashtagsScreen
+            )
+    {
+        super(nameScreen, locationScreen, photosScreen, hashtagsScreen);
+    }
 }
