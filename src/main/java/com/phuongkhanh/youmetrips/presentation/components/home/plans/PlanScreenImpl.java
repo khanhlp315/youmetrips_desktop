829613a058e2b9e4c023f23fd07597adba04c3c3
Lê Phương Khanh <khanhlp315@gmail.com>
2018-06-14 13:18:50 +0700

    fix some minor bugs

--- src/main/java/com/phuongkhanh/youmetrips/presentation/components/home/plans/PlanScreenImpl.java
+++ src/main/java/com/phuongkhanh/youmetrips/presentation/components/home/plans/PlanScreenImpl.java
@@ -14,6 +14,7 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import javax.inject.Inject;
@@ -31,7 +32,7 @@ implements PlanScreen, Initializable {
     private final Provider<CreatePlaceWindow> _placeWindow;
 
     @FXML
-    private JFXListView _lvPlans;
+    private ListView _lvPlans;
 
     @Override
     public Scene render() {
