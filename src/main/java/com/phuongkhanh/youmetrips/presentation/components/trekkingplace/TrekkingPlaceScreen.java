839217af3defa6c489f0f9943ab251f98343fadf
levogiakhang <levogiakhang@gmail.com>
2018-06-14 12:11:20 +0700

    remove trekking place

--- src/main/java/com/phuongkhanh/youmetrips/presentation/components/trekkingplace/TrekkingPlaceScreen.java
+++
@@ -1,8 +0,0 @@
-package com.phuongkhanh.youmetrips.presentation.components.trekkingplace;
-
-public interface TrekkingPlaceScreen {
-    void navigateBack();
-    void returnPlace(int placeId);
-    void showError(String title, String message);
-    void setLoading(boolean value);
-}
